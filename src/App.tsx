d6fcd4ca60157b4f6b74336dbd3ddc63abeaf99d
Sahil Chadha <Chadhas282@gmail.com>
2026-08-22 08:47:01 +0530

    Enhance LEVELS data; add truth table entries for additional 3-Input combinations

--- src/App.tsx
+++ src/App.tsx
@@ -3398,3 +3398,43 @@ export const LEVELS: Level[] = [
         ],
         "output": 0
       },
+      {
+        "inputs": [
+          0,
+          1,
+          1
+        ],
+        "output": 1
+      },
+      {
+        "inputs": [
+          1,
+          0,
+          0
+        ],
+        "output": 0
+      },
+      {
+        "inputs": [
+          1,
+          0,
+          1
+        ],
+        "output": 1
+      },
+      {
+        "inputs": [
+          1,
+          1,
+          0
+        ],
+        "output": 1
+      },
+      {
+        "inputs": [
+          1,
+          1,
+          1
+        ],
+        "output": 1
+      }
