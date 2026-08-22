8c4c267bde9e11c75cf4725f7b7932afcc55747b
Sahil Chadha <Chadhas282@gmail.com>
2026-08-22 08:48:00 +0530

    Enhance LEVELS data; add truth table entries for additional 4-input combinations

--- src/App.tsx
+++ src/App.tsx
@@ -3558,3 +3558,43 @@ export const LEVELS: Level[] = [
       },
       {
         "inputs": [
+           0,
+          0,
+          1,
+          1
+        ],
+        "output": 0
+      },
+      {
+        "inputs": [
+          0,
+          1,
+          0,
+          0
+        ],
+        "output": 0
+      },
+      {
+        "inputs": [
+          0,
+          1,
+          0,
+          1
+        ],
+        "output": 1
+      },
+      {
+        "inputs": [
+          0,
+          1,
+          1,
+          0
+        ],
+        "output": 0
+      },
+      {
+        "inputs": [
+          0,
+          1,
+          1,
+          1
